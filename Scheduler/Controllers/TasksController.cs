27afdb5c747325093958943c45261a53703528f4
Trevor <ngw21001@byui.edu>
2025-09-19 16:49:05 +0200

    Add task func

--- Scheduler/Controllers/TasksController.cs
+++ Scheduler/Controllers/TasksController.cs
@@ -1,6 +1,8 @@
 using System.Runtime.CompilerServices;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Scheduler.Data;
 using Scheduler.Models;
 using Scheduler.Models.Entities;
@@ -42,6 +44,50 @@ namespace Scheduler.Controllers
             await dbContext.SaveChangesAsync();
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TaskList()
+        {
+
+            var userId = User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            var tasks = await dbContext.TaskItems.ToListAsync();
+            List<TaskItem> usertasks = new List<TaskItem>();
+
+            foreach (var task in tasks)
+            {
+                if (task.UserId.ToString().Equals(userId))
+                {
+                    usertasks.Add(task);
+                }
+            }
+            return View(usertasks);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditTask(Guid id)
+        {
+            var task = await dbContext.TaskItems.FindAsync(id);
+
+            return View(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditTask(TaskItem viewModel)
+        {
+            var task = await dbContext.TaskItems.FindAsync(viewModel.Id);
+
+            if (task is not null)
+            {
+                task.Name = viewModel.Name;
+                task.Description = viewModel.Description;
+                task.DueDate = viewModel.DueDate;
+                task.Complete = viewModel.Complete;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("TaskList", "Tasks");
         }
     }
 }
